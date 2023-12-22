db775e8c2839f15b0532f2e303b0f9156682bca3
RianAina <128467312+RianAina@users.noreply.github.com>
2023-12-22 08:57:19 +0300

    Updated Sum.java

--- src/main/java/com/examen/poja/endpoint/rest/controller/health/Sum.java
+++ src/main/java/com/examen/poja/endpoint/rest/controller/health/Sum.java
@@ -1,7 +1,7 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.math.BigInteger;
 
@@ -13,11 +13,11 @@ public class SumAPI {
         SpringApplication.run(SumAPI.class, args);
     }
 
-    @GetMapping("/sum")
-    public String sum(@RequestParam("a") String a, @RequestParam("b") String b) {
-        BigInteger numA = new BigInteger(a);
-        BigInteger numB = new BigInteger(b);
-        BigInteger sum = numA.add(numB);
+    @GetMapping("/sum/{num1}/{num2}")
+    public String sum(@PathVariable("num1") String num1, @PathVariable("num2") String num2) {
+        BigInteger number1 = new BigInteger(num1);
+        BigInteger number2 = new BigInteger(num2);
+        BigInteger sum = number1.add(number2);
         return sum.toString();
     }
 }
